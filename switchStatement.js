d9bad30cf58bc26404adcd7a7d1e328d963c535e
samirrasul <samirrasul15oct@gmail.com>
2021-11-03 01:46:32 +0400

    switchStatement.js added

---
+++ switchStatement.js
@@ -0,0 +1,18 @@
+let m = 2 + 9
+
+switch  (m) {
+    case 4:
+        console.log("It is too small");
+        break;
+    case 5:
+        console.log("It is appropriate");
+        break;
+    case 6:
+        console.log("It is too long");
+        break;
+    default:
+        console.log("I don't know what it should be");
+}
+
+
+
