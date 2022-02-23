5a39a6cf136017c48c69895f009d0c493d453a7b
samirrasul <samirrasul15oct@gmail.com>
2022-02-23 23:47:41 +0400

    Objects added

--- objects.js
+++ objects.js
@@ -1,4 +1,16 @@
 let user = {
     name: "John",
-    age: 30
+    age: 30,
+    "like birds": true
 };
+
+console.log(user["like birds"])
+console.log(user.name)
+
+let key = "like birds"
+console.log(user[key])
+
+for (let a in user) {
+    console.log(a)
+    console.log(user[a])
+}
