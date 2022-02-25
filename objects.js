33f532e90e6fc7ce490a9c0efb682460d2663077
samirrasul <samirrasul15oct@gmail.com>
2022-02-25 14:26:09 +0400

    Objects added

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-let user = {
+/*let user = {
     name: "John",
     age: 30,
     "like birds": true
@@ -13,4 +13,46 @@ console.log(user[key])
 for (let a in user) {
     console.log(a)
     console.log(user[a])
+}*/
+/*let bag = {
+    "fruit" : 5
 }
+console.log(bag["fruit"])*/
+/*function makeUser(name, age, date) {
+    return {
+        name: name,
+        age: age,
+        birthdate : date
+    };
+}
+
+let user = makeUser("Samir", 47, 1975)
+console.log(user.birthdate + user.age)*/
+/*let user = {
+    5 : "test",
+}
+
+console.log(user[5])*/
+/*let codes = {
+     "1" : "USA",
+     44 : "Germany",
+     "994" : "Azerbaijan",
+     41 : "Switzerland",
+    name: "Random"
+}
+
+for (let key in codes) {
+        console.log(codes[key])
+}
+
+//delete (codes["1"])
+//console.log(delete(codes["1"]))*/
+/*let user = {
+    name : "John",
+    age : 20
+}
+
+let admin = user
+console.log(admin.age)
+admin.name = "Samir"
+console.log(admin.name)*/
