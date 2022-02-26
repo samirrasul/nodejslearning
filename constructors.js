cc294d06619984ec46ba0dbb5092a840fe209b91
samirrasul <samirrasul15oct@gmail.com>
2022-02-26 22:08:44 +0400

    Constructors added

---
+++ constructors.js
@@ -0,0 +1,25 @@
+/*function User (name) {
+    this.name = name;
+    this.isAdmin = false;
+}
+
+let user = new User("Jack")
+console.log(user.isAdmin)
+console.log(user.name)*/
+
+/*function BigUser() {
+    this.name = 'Jacob';
+    return this.name
+};
+
+console.log(new BigUser().name)*/
+
+/*function User(name) {
+    this.name = name;
+    this.sayHi = function () {
+        console.log('My name is ' + this.name)
+    }
+};
+
+let user = new User('Jacob')
+user.sayHi()*/
