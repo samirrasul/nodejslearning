38c07d8ac65af804b30fd0a6b56b50334df48be2
samirrasul <samirrasul15oct@gmail.com>
2022-02-26 23:29:03 +0400

    Optinal Chaining added

---
+++ optionalChaining.js
@@ -0,0 +1,21 @@
+/*let user = {}
+
+console.log(user.address?.street)*/
+
+/*let userAdmin = {
+    admin () {
+        console.log('I am admin');
+    }
+};
+let userGuest = {}
+userAdmin.admin?.()
+console.log(userGuest.admin?.())*/
+
+/*let user1 = {
+    name: 'Jacob'
+};
+
+let user2 = null;
+
+console.log(user1?.['name'])
+console.log(user2?.['name'])*/
