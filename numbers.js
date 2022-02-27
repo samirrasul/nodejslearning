4e98f9be30e60291e75735bd4584846323c16b79
samirrasul <samirrasul15oct@gmail.com>
2022-02-27 20:14:49 +0400

    Numbers added

---
+++ numbers.js
@@ -0,0 +1,24 @@
+/*let num = 123234234234234
+console.log(num.toString(36))
+console.log(123..toString(36))*/
+
+/*let num = 12.567
+console.log(Math.round(num*100)/100)
+console.log(+num.toFixed(5))
+console.log(+(0.1 + 0.2).toFixed(20))
+console.log(+0.1.toFixed(20))
+console.log(+('5'+'3'))
+console.log(-Infinity > 5)
+console.log(typeof NaN)
+console.log(isNaN('false'))
+console.log(+'100px')
+console.log(parseInt('100ef12'))
+console.log(parseInt('af', 16));
+console.log(Math.max(1, 23, 14, 99, 99))*/
+
+/*function random (a, b) {
+    let rand = a - 0.5 + Math.random() * (b - a + 1)
+    console.log(Math.round(rand))
+}
+
+random(0.111111,1.99999999)*/
