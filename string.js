6c3ac6d1974a7617973661f267b144a5737c0cd7
samirrasul <samirrasul15oct@gmail.com>
2022-03-02 17:10:31 +0400

    String added

---
+++ string.js
@@ -0,0 +1,67 @@
+/*function sum (a, b) {
+    return a + b;
+}
+//console.log(sum(1,2))
+//console.log(`1 + 2 = ${sum(1, 2)}`)
+/*console.log(`Guests:
+Samir
+Yalchin
+Jacob`)*/
+//console.log('\u{1F60D}')
+/*console.log(`sdfsdf`.length)
+let str = 'Jacob'
+console.log(str[3])*/
+
+/*let str = 'Jacob'
+//console.log(str[str.length - 1])
+for(let char of str) {
+    console.log(char)
+}*/
+
+/*let str = 'As sly as a fox, as strong as an ox';
+let target = 'as';
+let pos = 0;
+while (1) {
+    let foundpos = str.indexOf(target, pos);
+    if(foundpos == -1)  break;
+    console.log(`Address found at: ${foundpos}`);
+    pos = foundpos + 1;
+}*/
+
+/*let str = `Jacob Hossein`
+console.log(str.slice(4, 9))
+console.log(str.slice(-6, -1))
+console.log(str.substring(6, 0))*/
+
+/*let string = 'Az'
+console.log('Rəsulov'.codePointAt(1))*/
+
+/*function ucFirst (str) {
+    let UPPER = str.toUpperCase();
+    let FirstLetter = UPPER.slice(0, 1);
+    let lower = (UPPER.slice(1)).toLowerCase();
+    let FirstUpper = FirstLetter + lower;
+    console.log(FirstUpper);
+}
+ucFirst('jacob')*/
+
+/*function checkSpam (str) {
+    let lower = str.toLowerCase()
+    if(lower.includes('viagra') || lower.includes('xxx')) {
+        console.log(true);
+    }
+    else {
+        console.log(false);
+    }
+}
+checkSpam('buy xxxx now')*/
+
+/*function truncate(str, maxlength) {
+    if(str.length <= maxlength) {
+        console.log(str);
+    }
+    else {
+        console.log(str.slice(0, maxlength -1) + '...');
+    }
+}
+truncate('I have a pen', 1)*/
