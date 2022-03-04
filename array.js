f2d13a2a866ad768a08e5054e9758c085b1d1c87
samirrasul <samirrasul15oct@gmail.com>
2022-03-04 13:05:03 +0400

    Array added

---
+++ array.js
@@ -0,0 +1,24 @@
+/*let arr = ['Jacob', 'Hossein', 'Abdur'];
+//console.log(arr[0]);
+arr[2] = 'Yalchin';*/
+//console.log(arr);
+//console.log(arr.pop());
+//console.log(arr.length)
+//console.log(arr.unshift('Samir'))
+//console.log(arr);
+/*for(let name in arr) {
+    console.log(arr[name]);
+}*/
+/*arr.length = 2;
+console.log(arr);
+arr.length = 3;
+console.log(arr[3])*/
+/*let arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+console.log(arr) */
+/*let arr = [1, 2, 13];
+console.log(String(arr));*/
+//console.log([1,2] + 1)
