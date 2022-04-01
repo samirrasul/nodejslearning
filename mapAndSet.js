a59bbd43e692e2d6b0b7d008bbca5ab77927b4fd
samirrasul <samirrasul15oct@gmail.com>
2022-04-01 23:58:51 +0400

    Map and Set added

---
+++ mapAndSet.js
@@ -0,0 +1,21 @@
+/*function aclean(arr) {
+    let map = new Map();
+    for(let word of arr) {
+        let sorted = word.toLowerCase().split('').sort().join('');
+        map.set(sorted, word);
+    }
+    return Array.from(map.values());
+}
+let values = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+console.log(aclean(values));*/
+
+/*function aclean(arr) {
+    let map = new Map();
+    for(let word of arr) {
+        let sorted = word.toLowerCase().split('').sort().join('');
+        map.set(sorted, word);
+    }
+    return Array.from(map.values());
+}
+let val = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+console.log(aclean(val));*/
