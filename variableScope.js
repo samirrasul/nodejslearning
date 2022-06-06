dbe76bf8540347c6902220c106831a425c44b31e
samirrasul <samirrasul15oct@gmail.com>
2022-06-06 14:30:16 +0400

    Variable Scope added

---
+++ variableScope.js
@@ -0,0 +1,27 @@
+/*function sayHi(firstName, lastName) {
+    function sayMyName() {
+        return firstName + ' ' + lastName;
+    }
+    console.log("Hi" + ' ' + sayMyName());
+}
+
+sayHi('Jacob', 'Hossain')*/
+
+/*function makeCounter() {
+    let count = 0;
+    return function() {
+        return count++;
+    }
+}
+counter = makeCounter();
+console.log(counter());
+console.log(counter());*/
+
+function sum(a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c;
+        }
+    }
+}
+console.log(sum(2)(3)(4))
