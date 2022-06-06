c6f99e7596beb990bac4805435cb292a6f07d946
samirrasul <samirrasul15oct@gmail.com>
2022-06-06 17:50:54 +0400

    Tasks added

---
+++ tasks.js
@@ -0,0 +1,42 @@
+/*let pos = 0;
+for(let i=0; i<=5; i++ ) {
+    pos = pos + 1;
+    //console.log(pos);
+    console.log(i);
+    if(i == 4) {
+        break;
+    }
+}*/
+
+/*let pos = 0;
+let i = 0;
+while(i <= 5) {
+    pos = pos + 1;
+    if(i == 4) break;
+    console.log(i);
+    i++;
+}*/
+/*let str = 'As sly as a fox, as strong as an ox';
+let foundpos = str.indexOf('as');
+console.log(foundpos);*/
+
+/*function unique (arr) {
+    return Array.from(new Set(arr));
+}
+let values = ['Hare', 'Krishna', 'Hare'];
+console.log(unique(values));*/
+
+/*let prices = {
+    banana: 1,
+    orange: 2,
+    meat: 4,
+};
+
+let doublePrices = Object.fromEntries(
+    // convert prices to array, map each key/value pair into another pair
+    // and then fromEntries gives back the object
+    Object.entries(prices).map(entry => [entry[0], entry[1]])
+);
+
+console.log(doublePrices.banana); // 8*/
+
