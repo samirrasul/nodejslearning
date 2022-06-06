4f66ab58ec7553aa8fc5826f3584321e0590af84
samirrasul <samirrasul15oct@gmail.com>
2022-06-06 11:32:23 +0400

    Rest Parameters and Spread Syntax added

---
+++ restParameters.js
@@ -0,0 +1,26 @@
+/*function sumAll(...args) {
+    let sum = 0;
+    for(let arg of args) {
+        sum += arg;
+    }
+    return sum;
+}
+
+console.log(sumAll(1, 2, 3, 4))*/
+
+/*function name(firstName, lastName, ...titles) {
+    console.log(firstName + ' ' + lastName);
+    console.log(titles[0] = 'Monarch');
+    console.log(titles [1] = 'Great')
+}
+
+name('Julie', 'Caesar', 'Monarch', 'Great');*/
+
+/*let arr1 = [1, 10, 7, 13, 2];
+let arr2 = [1.4, 33, 42.1];
+merged = [...arr1, ...arr2];
+console.log(Math.max(...arr1, ...arr2));
+console.log(merged);*/
+
+let name = 'Jacob';
+console.log([...name]);
